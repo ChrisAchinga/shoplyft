bc791ef0206d5f083be77a9c44ec779c63321bef
ChrisAchinga <achinga.chris@gmail.com>
2021-03-31 12:45:32 +0300

    urls and djangorest

--- server/config/settings.py
+++ server/config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    # installed applications
+    'rest_framework',
     # my appslications
     'base.apps.BaseConfig',
 ]
