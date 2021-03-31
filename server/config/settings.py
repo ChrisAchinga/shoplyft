b325df52f2f72878ffd0bad77dea2f4c2c01179a
ChrisAchinga <achinga.chris@gmail.com>
2021-03-31 12:19:21 +0300

    setup django app

--- server/config/settings.py
+++ server/config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my appslications
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
